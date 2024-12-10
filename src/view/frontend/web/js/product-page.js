65094a262c80c2df20c528e4ea673e5b3bfc1a53
ah-net <103565001+ah-net@users.noreply.github.com>
2024-12-10 14:25:48 +0100

    Change working to use internal analytics endpoint

--- src/view/frontend/web/js/product-page.js
+++ src/view/frontend/web/js/product-page.js
@@ -1,38 +1,18 @@
 function Tweakwise_TweakwiseHyva_Pageview(data) {
     return {
-        apiUrl: data.apiUrl,
-        tweakwiseCookieName: data.tweakwiseCookieName,
         productKey: data.productKey,
-        instanceKey: data.instanceKey,
-        getCookie(name) {
-            const value = `; ${document.cookie}`;
-            const parts = value.split(`; ${name}=`);
-            if (parts.length === 2) return parts.pop().split(';').shift();
-        },
-        setCookie(name, value, days) {
-            const date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            const expires = `expires=${date.toUTCString()}`;
-            document.cookie = `${name}=${value}; ${expires}; path=/`;
-        },
         init() {
-            var profileKey = this.getCookie(this.tweakwiseCookieName);
-            if (!profileKey) {
-                profileKey = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-                this.setCookie(this.tweakwiseCookieName, profileKey, 365);
-            }
-
-            fetch(this.apiUrl, {
+            fetch('/tweakwise/ajax/analytics', {
                 method: 'POST',
                 headers: {
-                    'Instance-Key': this.instanceKey,
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'X-Requested-With': 'XMLHttpRequest'
                 },
                 body: JSON.stringify({
-                    profileKey: profileKey,
+                    type: 'product',
                     productKey: this.productKey
                 })
-            }).catch(error => console.error('Tweakwise API call failed:', error));
+            }).catch(error => console.error('Tweakwise API call failed:', error.message));
         }
     }
 }
--- src/view/frontend/web/js/search-page.js
+++ src/view/frontend/web/js/search-page.js
@@ -1,35 +1,16 @@
 function Tweakwise_Hyva_SearchPage(config) {
     return {
-        apiUrl: config.apiUrl,
         searchQuery: config.searchQuery,
-        instanceKey: config.instanceKey,
-        tweakwiseCookieName: config.tweakwiseCookieName,
-        getCookie(name) {
-            const value = `; ${document.cookie}`;
-            const parts = value.split(`; ${name}=`);
-            if (parts.length === 2) return parts.pop().split(';').shift();
-        },
-        setCookie(name, value, days) {
-            const date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            const expires = `expires=${date.toUTCString()}`;
-            document.cookie = `${name}=${value}; ${expires}; path=/`;
-        },
         init() {
-            var profileKey = this.getCookie(this.tweakwiseCookieName);
-            if (!profileKey) {
-                profileKey = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-                this.setCookie(this.tweakwiseCookieName, profileKey, 365);
-            }
 
-            fetch(this.apiUrl, {
+            fetch('/tweakwise/ajax/analytics', {
                 method: 'POST',
                 headers: {
-                    'Instance-Key': this.instanceKey,
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'X-Requested-With': 'XMLHttpRequest'
                 },
                 body: JSON.stringify({
-                    profileKey: profileKey,
+                    type: 'search',
                     searchTerm: this.searchQuery
                 })
             }).catch(error => console.error('Tweakwise API call failed:', error));
