0d42e077fe26fb359778675ae14d8f9931e20ea3
Pascal Wientjes <pwientjes@emico.nl>
2026-06-04 11:07:35 +0200

    remove unnecessary check for DOM node type

--- src/view/frontend/web/js/analytics.js
+++ src/view/frontend/web/js/analytics.js
@@ -49,10 +49,6 @@ function handleItemClick(event, config) {
                 return;
             }
 
-            if (event.target.nodeName !== 'IMG' && event.target.nodeName !== 'A') {
-                return;
-            }
-
             const product = event.target.closest(config.productSelector);
             let productId;
 
